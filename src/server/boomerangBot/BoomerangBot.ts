2ca95926d956ebc249bcfc82e3efc43b61cb8caa
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-06-13 05:16:56 +0300

    linting

--- src/server/boomerangBot/BoomerangBot.ts
+++ src/server/boomerangBot/BoomerangBot.ts
@@ -1,100 +1,97 @@
-import { TurnContext, ActivityTypes, BotFrameworkAdapter, CardFactory } from 'botbuilder';
-import { AdaptiveCard } from 'adaptivecards';
-import { ConversationState, MemoryStorage, UserState } from 'botbuilder';
+import { TurnContext, ActivityTypes, BotFrameworkAdapter, CardFactory, ConversationState, MemoryStorage, UserState } from "botbuilder";
+import { AdaptiveCard } from "adaptivecards";
 
-import { DialogBot } from './dialogBot';
-import { MainDialog } from './dialogs/mainDialog';
-import WelcomeCard from './cards/welcomeCard';
-import ReminderMessageExtension from '../reminderMessageExtension/ReminderMessageExtension';
-import { MessageExtensionDeclaration, BotDeclaration, PreventIframe } from 'express-msteams-host';
+import { DialogBot } from "./dialogBot";
+import { MainDialog } from "./dialogs/mainDialog";
+import WelcomeCard from "./cards/welcomeCard";
+import ReminderMessageExtension from "../reminderMessageExtension/ReminderMessageExtension";
+import { MessageExtensionDeclaration, BotDeclaration, PreventIframe } from "express-msteams-host";
 
 @BotDeclaration(
-  '/api/messages',
-  new MemoryStorage(),
-  process.env.MICROSOFT_APP_ID,
-  process.env.MICROSOFT_APP_PASSWORD
+    "/api/messages",
+    new MemoryStorage(),
+    process.env.MICROSOFT_APP_ID,
+    process.env.MICROSOFT_APP_PASSWORD
 )
-@PreventIframe('/boomerangBot/aboutBoomerang.html')
-@MessageExtensionDeclaration('reminderMessageExtension')
+@PreventIframe("/boomerangBot/aboutBoomerang.html")
+@MessageExtensionDeclaration("reminderMessageExtension")
 export class BoomerangBot extends DialogBot {
-  private adapter: BotFrameworkAdapter;
-  private reminderMessageExtension: ReminderMessageExtension;
+    private adapter: BotFrameworkAdapter;
+    private reminderMessageExtension: ReminderMessageExtension;
 
-  constructor(conversationState: ConversationState, userState: UserState) {
-    super(conversationState, userState, new MainDialog());
-    this.adapter = new BotFrameworkAdapter();
-    this.reminderMessageExtension = new ReminderMessageExtension();
+    constructor(conversationState: ConversationState, userState: UserState) {
+        super(conversationState, userState, new MainDialog());
+        this.adapter = new BotFrameworkAdapter();
+        this.reminderMessageExtension = new ReminderMessageExtension();
 
-    this.onMessage(async (context: TurnContext) => {
-      const messageId = context.activity.id;
-      const conversationId = context.activity.conversation.id;
+        this.onMessage(async (context: TurnContext) => {
+            const messageId = context.activity.id;
+            const conversationId = context.activity.conversation.id;
 
-      const replied = await this.checkMessageReplied(messageId);
-      const replyLater = await this.checkMessageReplyLater(context);
+            const replied = await this.checkMessageReplied(messageId);
+            const replyLater = await this.checkMessageReplyLater(context);
 
-      if (!replied && !replyLater) {
-        await this.sendReminder(context, messageId, conversationId);
-      }
+            if (!replied && !replyLater) {
+                await this.sendReminder(context, messageId, conversationId);
+            }
 
-      await super.onMessage(context);
-    });
+            await super.onMessage(context);
+        });
 
-    this.onMembersAdded(async (context, next) => {
-      const membersAdded = context.activity.membersAdded;
-      if (membersAdded && membersAdded.length > 0) {
-        for (let cnt = 0; cnt < membersAdded.length; cnt++) {
-          if (membersAdded[cnt].id !== context.activity.recipient.id) {
-            await this.sendWelcomeCard(context);
-          }
-        }
-      }
-      await next();
-    });
-  }
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            if (membersAdded && membersAdded.length > 0) {
+                for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+                    if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                        await this.sendWelcomeCard(context);
+                    }
+                }
+            }
+            await next();
+        });
+    }
 
-  private async checkMessageReplied(messageId: string): Promise<boolean> {
+    private async checkMessageReplied(messageId: string): Promise<boolean> {
     // Implement logic to check if the message has been replied to
     // You can use external storage or database to track message replies
 
-    // For demonstration purposes, assume the message has been replied to
-    return true;
-  }
+        // For demonstration purposes, assume the message has been replied to
+        return true;
+    }
 
-  private async checkMessageReplyLater(context: TurnContext): Promise<boolean> {
+    private async checkMessageReplyLater(context: TurnContext): Promise<boolean> {
     // Implement logic to check if the message has been marked for "reply later"
     // You can use user or conversation state to track the "reply later" status
 
-    // For demonstration purposes, assume the message has been marked for "reply later"
-    return true;
-  }
+        // For demonstration purposes, assume the message has been marked for "reply later"
+        return true;
+    }
 
-  private async sendReminder(context: TurnContext, messageId: string, conversationId: string) {
-    const reminderMessage = `You have an unread message: ${messageId}`;
+    private async sendReminder(context: TurnContext, messageId: string, conversationId: string) {
+        const reminderMessage = `You have an unread message: ${messageId}`;
 
-    // Create an Adaptive Card for the reminder message
-    const adaptiveCard = new AdaptiveCard();
-    adaptiveCard.addTextBlock(reminderMessage);
+        // Create an Adaptive Card for the reminder message
+        const adaptiveCard = new AdaptiveCard();
+        adaptiveCard.addTextBlock(reminderMessage);
 
-    // Convert the Adaptive Card to an attachment
-    const cardAttachment = CardFactory.adaptiveCard(adaptiveCard);
+        // Convert the Adaptive Card to an attachment
+        const cardAttachment = CardFactory.adaptiveCard(adaptiveCard);
 
-    // Send the reminder message to the user or channel
-    const reminderActivity = {
-      type: ActivityTypes.Message,
-      attachments: [cardAttachment],
-      conversation: { id: conversationId },
-      recipient: { id: context.activity.from.id },
-    };
+        // Send the reminder message to the user or channel
+        const reminderActivity = {
+            type: ActivityTypes.Message,
+            attachments: [cardAttachment],
+            conversation: { id: conversationId },
+            recipient: { id: context.activity.from.id }
+        };
 
-    await this.adapter.continueConversation(conversationId, async (turnContext) => {
-      await turnContext.sendActivity(reminderActivity);
-    });
-  }
+        await this.adapter.continueConversation(conversationId, async (turnContext) => {
+            await turnContext.sendActivity(reminderActivity);
+        });
+    }
 
-  public async sendWelcomeCard(context: TurnContext): Promise<void> {
-    const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
-    await context.sendActivity({ attachments: [welcomeCard] });
-  }
+    public async sendWelcomeCard(context: TurnContext): Promise<void> {
+        const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
+        await context.sendActivity({ attachments: [welcomeCard] });
+    }
 }
-
-
--- src/server/boomerangBot/dialogBot.ts
+++ src/server/boomerangBot/dialogBot.ts
@@ -1,46 +1,46 @@
 import {
-  ConversationState,
-  UserState,
-  TeamsActivityHandler,
-  TurnContext,
-  StatePropertyAccessor,
-} from 'botbuilder';
-import { MainDialog } from './dialogs/mainDialog';
-import { DialogSet } from 'botbuilder-dialogs';
+    ConversationState,
+    UserState,
+    TeamsActivityHandler,
+    TurnContext,
+    StatePropertyAccessor
+} from "botbuilder";
+import { MainDialog } from "./dialogs/mainDialog";
+import { DialogSet } from "botbuilder-dialogs";
 
 export class DialogBot extends TeamsActivityHandler {
-  private conversationState: ConversationState;
-  private userState: UserState;
-  private dialog: MainDialog;
-  private dialogState: StatePropertyAccessor<any>;
+    private conversationState: ConversationState;
+    private userState: UserState;
+    private dialog: MainDialog;
+    private dialogState: StatePropertyAccessor<any>;
 
-  constructor(conversationState: ConversationState, userState: UserState, dialog: MainDialog) {
-    super();
-    this.conversationState = conversationState;
-    this.userState = userState;
-    this.dialog = dialog;
-    this.dialogState = this.conversationState.createProperty('DialogState');
+    constructor(conversationState: ConversationState, userState: UserState, dialog: MainDialog) {
+        super();
+        this.conversationState = conversationState;
+        this.userState = userState;
+        this.dialog = dialog;
+        this.dialogState = this.conversationState.createProperty("DialogState");
 
-    this.onMessage(async (context: TurnContext, next: () => Promise<void>) => {
-      const dialogContext = await this.dialog.createContext(context);
+        this.onMessage(async (context: TurnContext, next: () => Promise<void>) => {
+            const dialogContext = await this.dialog.createContext(context);
 
-      // Continue the dialog if it's not done
-      if (!dialogContext.context.responded) {
-        await dialogContext.continueDialog();
-      }
+            // Continue the dialog if it's not done
+            if (!dialogContext.context.responded) {
+                await dialogContext.continueDialog();
+            }
 
-      // Start the dialog if it hasn't been started yet
-      if (!context.responded) {
-        await dialogContext.beginDialog(this.dialog.id);
-      }
+            // Start the dialog if it hasn't been started yet
+            if (!context.responded) {
+                await dialogContext.beginDialog(this.dialog.id);
+            }
 
-      await next();
-    });
-  }
+            await next();
+        });
+    }
 
-  public async run(context: TurnContext): Promise<void> {
-    await super.run(context);
-    await this.conversationState.saveChanges(context, false);
-    await this.userState.saveChanges(context, false);
-  }
+    public async run(context: TurnContext): Promise<void> {
+        await super.run(context);
+        await this.conversationState.saveChanges(context, false);
+        await this.userState.saveChanges(context, false);
+    }
 }
